fb0f58cb99ad091124dc5dbd1994add962e5cb0b
melorana <ariadna071107@gmail.com>
2025-12-23 20:45:45 +0300

    Delete laba1 directory

--- laba1/task2.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Введите первое число: "))
-s = int(input("Введите второе число: "))
-if num > s:
-    print("Большее число:", num)
-elif s > num:
-    print("Большее число:", s )
-elif num==s:
-    print("Числа одинаковы")
