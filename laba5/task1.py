6478b71b31b1016524a44ba6e4bba3d4f3212ca4
melorana <ariadna071107@gmail.com>
2025-12-23 20:46:40 +0300

    Delete laba5 directory

--- laba5/task1.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
-    def get_info(self):
-        return f'Название книги: {self.title}, Автор: {self.author}, Год издания: {self.year}'
-title = Book("Преступление и наказание", "Ф.М. Достоевский", 1866)
-print(title.get_info())
